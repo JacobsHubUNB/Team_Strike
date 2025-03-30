665e864b5178c2acc85d09574afd6049b86a0daa
zynsniper <zynsniper@gmail.com>
2025-03-30 14:04:35 -0300

    removed palace instances

--- src/map_function.c
+++ src/map_function.c
@@ -1,7 +1,8 @@
-#include "map_function.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "map_function.h"
+#include "defender.h"
 
 // Function definition
 void printMap(Tile gameMap[10][10]) {
@@ -19,26 +20,12 @@ void generateMap(Tile gameMap[10][10]){
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             gameMap[i][j].type = '.'; 
-            if(i ==5 && j == 5){
+            if(i == 5 && j == 5){
                 gameMap[i][j].type = 'P';
-                gameMap[i][j].palace = (Palace *)malloc(sizeof(Palace));
-                if(!gameMap[5][5].palace){
-                    printf("Palace malloc failed");   
-                }
-                else{
-                    gameMap[i][j].palace->health = 25;
-                    gameMap[i][j].palace->numDefender = 2;
-                    addDefender(gameMap[i][j].palace, 10);
-                    
-                }
             }
         }
     }
-   for(int i = 0; i < (gameMap[5][5].palace->numDefender); i++){
-        int X = gameMap[5][5].palace->defender[i].pos[0];
-        int Y = gameMap[5][5].palace->defender[i].pos[1];
-        gameMap[Y][X].type = '8';
-   }
+
     int obstacleCount = 20;
     int placedObstacles = 0;
     while(placedObstacles <= obstacleCount){
@@ -50,5 +37,4 @@ void generateMap(Tile gameMap[10][10]){
             placedObstacles++;
         }
     }
-
 }
