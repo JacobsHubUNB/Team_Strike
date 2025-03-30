cb96ca3e99cd13c18eb46944e11ac4c2910e3017
zynsniper <zynsniper@gmail.com>
2025-03-30 14:05:24 -0300

    removed attack_palace method header

--- header/move_function.h
+++ header/move_function.h
@@ -8,7 +8,6 @@ bool moveRight(Team* team1, Team* team2,  Tile gameMap [10][10], int character);
 bool moveLeft(Team* team, Team* team2, Tile gameMap [10][10], int character);
 bool moveUp(Team* team, Team* team2, Tile gameMap [10][10], int character);
 bool moveDown(Team* team, Team* team2, Tile gameMap [10][10], int character);
-void attack(Character * attacker, Character * defender);
-int attack_palace(Palace* palace, Character * attacker);
+void attack(Character * attacker, Character * defender, Tile gameMap[10][10]);
 
 #endif
